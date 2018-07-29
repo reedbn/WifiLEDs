dc8fa1af7e732ac674484fa43af8009495c56451
Reed Nightingale <reed.nightingale@gmail.com>
2018-07-28 19:38:02 -0700

    Move character strings back, using static

--- config.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * This is where all the project-specific variables go
- * that tell all the hardware how to set up
- */
-
-const char* ssid = "RFish";
-const char* pass = "NotUrFish";
-
--- config.h
+++ config.h
@@ -12,8 +12,8 @@ const uint8_t LEDdataPin = 2;
 const uint8_t numLEDs = 20;
 const uint8_t maxSeqLen = 11;
 
-extern const char* ssid;//Compiler gets fussy if this is defined here. See config.cpp
-extern const char* pass;//Compiler gets fussy if this is defined here. See config.cpp
+static const char* ssid = "RFish";
+static const char* pass = "NotUrFish";
 const IPAddress apIP(192,168,4,1);
 const uint32_t STRIP_MAX_REFRESH_HZ = 60;
 
@@ -22,3 +22,4 @@ const uint8_t MAX_NUM_USER_SETTINGS = 3;
 #define PRINT_DEBUGGING_LED (0)
 #define PRINT_DEBUGGING_WIFLY (0)
 #define PRINT_DEBUGGING_WIFLY_DETAIL (0)
+#define PRINT_DEBUGGING_SETTINGS (0)
